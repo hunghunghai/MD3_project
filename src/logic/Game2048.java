6312b6dbc8fe2a3f9a7b69641186a853fbaa06ed
hunghunghai <phuongpidz@gmail.com>
2023-07-07 22:41:20 +0700

    .

--- src/logic/Game2048.java
+++ src/logic/Game2048.java
@@ -5,7 +5,7 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;zz
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game2048 {
